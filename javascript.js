6381214b2643f7727fb3e1de60a934dfb3120106
Justin <justinrkinsman@gmail.com>
2022-05-08 14:40:50 -0300

    Add basic functionality to game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 /*Rock, Paper, Scissors game*/
 
-//let playerSelection = prompt('Make your selection')
+
+
+function playRound() {
+    let playerSelection = prompt('Make your selection')
+    let computerChoices = ['Rock', 'Paper', 'Scissors']
+    let computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    if (playerSelection == computerSelection){
+    console.log('Tie game')
+    }else if ((playerSelection === 'Rock' && computerSelection === "Scissors") || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+    }else{
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+playRound()
