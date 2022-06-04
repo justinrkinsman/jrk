6e2344d421cdba33c83f40f2e89e30de614cac06
Justin <justinrkinsman@gmail.com>
2022-06-04 16:25:10 -0300

    Add dark mode and light mode buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+function darkMode() {
+    let element = document.body;
+    element.className = 'dark-mode'
+}
+function lightMode() {
+    let element = document.body;
+    element.className = 'light-mode'
+}
+
 /*Rock, Paper, Scissors game*/
 let playerSelection = document.querySelectorAll('button')
 
